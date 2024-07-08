c468f33969cd412108d82984f4824b3b793860d3
VictorJGB <victorjgb@gmail.com>
2024-07-08 08:52:48 -0300

    feat: Eslint configs

--- src/app/screens/Home/index.tsx
+++ src/app/screens/Home/index.tsx
@@ -1,10 +1,10 @@
-import { Text } from "react-native";
-import { Container } from "./styles";
+import { Text } from 'react-native'
+import { Container } from './styles'
 
 export default function Home() {
   return (
     <Container>
       <Text>Home Page!</Text>
     </Container>
-  );
+  )
 }
