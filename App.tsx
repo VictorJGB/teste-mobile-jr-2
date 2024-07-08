5ba4ab9beadc3994cdefa9fabf166024f41b29f8
Victor Jerrysson <victorjerrysson@gmail.com>
2024-07-07 22:49:08 -0300

    feat: initial commit

---
+++ App.tsx
@@ -0,0 +1,5 @@
+import Home from "@/app/home";
+import React from "react";
+export default function App() {
+  <Home />;
+}
---
+++ src/app/home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home Page!</Text>
+    </View>
+  );
+}
