8f3948b3ad621dca0fc2fe91feac302fa918f158
Victor Jerrysson <victorjerrysson@gmail.com>
2024-07-11 20:39:03 -0300

    feat: Creating the Book component layout

--- src/app/screens/Bookings/components/UpcomingBooking/index.tsx
+++ src/app/screens/Bookings/components/UpcomingBooking/index.tsx
@@ -1,5 +1,79 @@
-import { View } from "react-native";
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function UpComingBooking() {
-  return <View></View>;
+// types
+import Book from "@/app/types/Book";
+// expo
+import { Image } from "expo-image";
+
+// icons
+import { Feather } from "@expo/vector-icons";
+
+type Props = {
+  data: Book;
+};
+
+export default function UpComingBooking({ data }: Props) {
+  return (
+    <View>
+      {/* header */}
+      <View>
+        {/* image container */}
+        <View>
+          <Image
+            style={{ width: 30 }}
+            source={require("@/assets/svg/ac-category")}
+          />
+        </View>
+
+        {/* infos */}
+        <View>
+          <Text>{data.service.title}</Text>
+          <Text>Reference Code:{data.referenceCode}</Text>
+        </View>
+      </View>
+
+      {/* divider */}
+      <View />
+
+      {/* body */}
+      <View>
+        {/* body header */}
+        <View>
+          <Text>Status</Text>
+          <Text>{data.status}</Text>
+        </View>
+
+        {/* schedule */}
+        <View>
+          <View>
+            <Feather name="calendar" size={24} color="black" />
+          </View>
+          <View>
+            <Text>{data.schedule}</Text>
+            <Text>Schedule</Text>
+          </View>
+        </View>
+
+        {/* provider */}
+        <View>
+          {/* logo */}
+          <View>
+            <Image
+              style={{ width: 25 }}
+              source={require(data.service.provider.logo)}
+            />
+          </View>
+          <View>
+            <Text>{data.service.provider.name}</Text>
+            <Text>Service provider</Text>
+          </View>
+          <TouchableOpacity>
+            <Feather name="phone" size={24} color="black" />
+            <Text>call</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 }
